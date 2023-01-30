8c08acb886f9a00d2a9d4ccf95e8b0217102fa12
ginitadavis <120149064+ginitadavis@users.noreply.github.com>
2023-01-30 18:42:30 -0500

    Deleted comments that came from github

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 
 //Get current date, format it, and show it in the page
 var currentDate = dayjs();
@@ -70,19 +49,6 @@ $("textarea").click(function(){
 $("textarea").on("blur", function(){
   $(this).removeClass("editable");
 })
-
-
-//After saving the data with the buttons, the values of the textareas are saved in
-//the localStorage
-
-/*$(document).ready(function(){
-  console.log("hello 2")
-  $("#"+this.id).click(function(){
-    console.log("hello 3")
-    console.log(this.id)
-    window.alert("Button " + this.id + " was clicked");
-  });
-})*/
 
 //checks that the document is loaded, then when there's a click in one of the buttons
 //I send the information in the textarea to the localStorage. This happens for each button
@@ -163,12 +129,12 @@ $("textarea").each(function(event){
   console.log(rowNo[1]);
   console.log(currentHour);
 
+  //go through the rows and assign the colors of past, present, and future
   if (currentHour < rowNo[1]){
     console.log("Inside future Row No. "+rowNo[1].trim());
     console.log("Inside future current hr "+currentHour);
 
     $("textarea[name='text-"+rowNo[1]+"']").addClass("future");
-    //console.log("future "+"textarea[name='text-"+rowNo[1]+"']");
     console.log("future "+rowNo[1]);
 
   } else if (currentHour > rowNo[1]){
